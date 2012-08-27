5d27354a115cd5883e7e01693d7606c565bba2c5
Quinn Ebert <quinn@quinnebert.net>
2012-08-27 11:24:20 -0400

    Commit Code
    That's all, folks! ;-)

---
+++ README.md
@@ -0,0 +1,9 @@
+AtViaWeb is a trivial (e.g. user friendly) interface for managing "At" jobs on a UNIX/Linux system as whatever user the web server may happen to be running as.
+
+Don't complain to me about it being web-based and all the security and user-pitfall issues related to that.  It was developed specifically for a user who understood those limitations (and was gracious enough to let me retain the rights to it).
+
+Things I might do with it at some point:
+ * Output results of various system(), exec(), and or passthru() calls made by the script.
+ * Restrict access using a rudimentary password/username pair.
+
+This one's a "big-time freebie," so, don't expect too much to be done with it.  I got it done in one morning, also, so it probably has some gremlins.
---
+++ index.php
@@ -0,0 +1,90 @@
+<html>
+<head>
+<title>At via Web (Web Command Scheduler)</title>
+</head>
+<body>
+<h1 align="center">At via Web</h1>
+<h2 align="center">Web Command Scheduler</h2>
+<?php
+if (isset($_REQUEST['action']) && strlen($_REQUEST['action'])) {
+  if ($_REQUEST['action']=='Add Job') {
+    /*** Add an At job ***/
+    // Sanitize single quotes in the scheduled command (WARNING: a best effort):
+    $in_exec = str_replace("'","'\"'\"'",$_REQUEST['jobexec']);
+    // Run the command, store the output (we might output it in a future build):
+    $in_time = $_REQUEST['jobtime'];
+    $add_out = `echo '$in_exec' | at $in_time`;
+  } elseif ($_REQUEST['action']=='Delete Job') {
+    /*** Remove an At job ***/
+    $in_rmid = $_REQUEST['jobID'];
+    $add_out = `atrm $in_rmid`;
+  } else {
+    // A header to denote output of our issue(s):
+    echo('<h3 align="center">Your Last Request</h3>'."\n");
+    echo("<table border=\"0\" cellspacing=\"0\" cellpadding=\"0\" align=\"center\"><tr><td valign=\"middle\" align=\"center\">Action &quot;{$_REQUEST['action']}&quot; not supported!</i></td></tr></table>\n");
+  }
+}
+?>
+<h3 align="center">Your Scheduled Jobs</h3>
+<?php
+// Fetch the list of `at` jobs:
+$results = trim(`atq`);
+if (strlen($results)) {
+  // Excellent, we have jobs, prepare to parse them:
+  $results = explode("\n",$results);
+  // Parse through the fetched list, make it useful:
+  foreach ($results as $index => $value) {
+    $theJobs[$index] = explode("\t",$value);
+    $theJobs[$index][1] = explode(" ",$theJobs[$index][1]);
+    /*
+     * This results in an array with the following "parsed" format:
+     * 
+     * [0]    = Job ID
+     * [1][0] = Scheduled Date
+     * [1][1] = Scheduled Time
+     * [1][2] = (ignore this...) <= normally will be "a"
+     * [1][3] = (ignore this...) <= should be your username
+     */
+  }
+  // Output a table header:
+  echo('<table align="center" border="1" cellspacing="1" cellpadding="4"><tr><td><b>Job ID</b></td><td><b>Scheduled Date</b></td><td><b>Scheduled Time</b></td><td><b>Command Set</b></td><td><b>Job Actions</b></td></tr>');
+  // Output the list (if we have any jobs):
+  foreach ($theJobs as $atJob) {
+    // Job information:
+    echo('<tr><td>'.$atJob[0].'</td><td>'.$atJob[1][0].'</td><td>'.$atJob[1][1].'</td><td><i>Available in future versions...</i></td>');
+    // Job actions:
+    echo('<td><form style="margin-bottom: 0px; margin-top: 0px;" action="'.basename(__FILE__).'" method="POST"><input type="hidden" name="jobID" value="'.$atJob[0].'" /><input style="margin-bottom: 0px; margin-top: 0px;" type="submit" name="action" value="Delete Job" /></form></td>');
+    // End of table row:
+    echo('</tr>');
+  }
+  // Output table closing tag:
+  echo('</table>');
+} else {
+  /* No jobs, let the user know: */
+  // Output a table header:
+  echo('<table align="center" border="1" cellspacing="1" cellpadding="4" width="30%">');
+  // Output a "no jobs scheduled" message:
+  echo('<tr><td align="center"><b><i>No jobs currently scheduled!</i></b></td></tr>');
+  // Output table closing tag:
+  echo('</table>');
+}
+?>
+<h3 align="center">Schedule New Job</h3>
+<table align="center" border="1" cellspacing="1" cellpadding="4" width="25%">
+<tr>
+<td align="center" valign="middle">
+  <form style="margin-bottom: 0px;" action="<?php echo(basename(__FILE__)); ?>" method="POST">
+    <label for="jobtime">At Job Time Specification:</label><br />
+    <input type="text" name="jobtime" id="jobtime" size="32" /><br />
+    <label for="jobexec">Run Command:</label><br />
+    <input type="text" name="jobexec" id="jobexec" size="32" /><br />
+    <input type="hidden" name="action" value="Add Job" />
+    <input style="margin-bottom: 0px;" type="submit" name="Add Job" value="Add Job" />
+  </form>
+</td>
+</tr>
+</table>
+<h4 align="center" style="margin-bottom: 0px;">At via Web is open-source software</h4>
+<h5 align="center" style="margin-top: 0px;">by <a href="http://www.quinnebert.net/">Quinn &quot;Zed&quot; Ebert</a></h5>
+</body>
+</html>
